17de7bf7ece20882c1373aaa429a0ec44bd5003d
martin-242 <martin242@gmail.com>
2023-11-14 17:09:18 +0100

    Adds comments

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ function gridMaker(size){
     );
 };
 
+// Removes grid
 function removeGrid(){
     let allPixels = document.querySelectorAll("#gridPixel");
     allPixels.forEach(element => { 
@@ -27,6 +28,7 @@ function removeGrid(){
         });
 };
 
+// Clears grid
 function clearGrid(){
     let allPixels = document.querySelectorAll("#gridPixel");
     allPixels.forEach(element => { 
@@ -34,6 +36,7 @@ function clearGrid(){
         });
 };
 
+// Changes brush color
 function changeColor(color){
     let newColor = color;
     let allPixels = document.querySelectorAll("#gridPixel");    
@@ -48,43 +51,52 @@ function changeColor(color){
 
 // Program starts running here 
 
+// Sets default starting color
 let currentColor = "pixel_black";
 
+// Selects the grid containers
 const gridContainer = document.querySelector("#grid_container");
 const gridEnvelope = document.querySelector("#grid_envelope");
 
+// Sets the grid to 16x16
 const gridButton16 = document.querySelector("#button_16");
 gridButton16.addEventListener("click", () => {    
     removeGrid();
     gridMaker(16);
 });
 
+// Sets the grid to 32x32
 const gridButton32 = document.querySelector("#button_32");
 gridButton32.addEventListener("click", () => {
     removeGrid();
     gridMaker(32);
 });
 
+// Sets the grid to 64x64
 const gridButton64 = document.querySelector("#button_64");
 gridButton64.addEventListener("click", () => {
     removeGrid();
     gridMaker(64);
 });
 
+// Clears the grid
 const gridButtonClear = document.querySelector("#button_clear");
 gridButtonClear.addEventListener("click", () => {    
     clearGrid();
 });
 
+// Selects the eraser brush
 const gridButtonEraser = document.querySelector("#button_eraser");
 gridButtonEraser.addEventListener("click", () => {    
     changeColor("pixel_eraser");
     
 });
 
+// Selects the black brush
 const gridButtonBlack = document.querySelector("#button_black");
 gridButtonBlack.addEventListener("click", () => {    
     changeColor("pixel_black");
 });
 
+// Creates starting grid
 gridMaker(32);

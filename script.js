5dde26480955559591b47bb94fb7670612b21220
martin-242 <martin242@gmail.com>
2023-11-15 12:54:59 +0100

    Adds rainbow bush and code optimizations

--- script.js
+++ script.js
@@ -13,11 +13,7 @@ function gridMaker(size){
         gridPixel.setAttribute("id", "gridPixel");
         gridPixel.setAttribute("class", ("pixel_" + `${i}`));
     };
-    let allPixels = document.querySelectorAll("#gridPixel");    
-    allPixels.forEach(element => { element.addEventListener("mouseover", () => { 
-        element.classList.add(`pixel_black`); } )
-    }
-    );
+    changeColor(currentColor);
 };
 
 // Removes grid
@@ -32,24 +28,66 @@ function removeGrid(){
 function clearGrid(){
     let allPixels = document.querySelectorAll("#gridPixel");
     allPixels.forEach(element => { 
+        element.removeAttribute("style");
         element.classList.remove("pixel_black");
+        element.classList.remove("pixel_eraser");
         });
 };
 
 // Changes brush color
 function changeColor(color){
     let newColor = color;
+    if ( newColor == "pixel_rainbow" ) {
+        const rainbowColors = ["#e81416", "#ffa500", "#faeb36", "#79c314", "#487de7", "#4b369d", "#70369d"];        
+        let allPixels = document.querySelectorAll("#gridPixel");    
+        allPixels.forEach(element => { element.addEventListener("mouseover", () => { 
+            let randomNumber = Math.floor(Math.random() * 7);
+            element.classList.remove(`${currentColor}`); 
+            element.classList.add(`${newColor}`);
+            let randomColor = rainbowColors[randomNumber];
+            element.setAttribute("style", `background-color:${randomColor}`);
+            currentColor = newColor;
+        } ) } );
+        return currentColor;
+    }
+    else {
     let allPixels = document.querySelectorAll("#gridPixel");    
     allPixels.forEach(element => { element.addEventListener("mouseover", () => { 
+        element.removeAttribute("style");
         element.classList.remove(`${currentColor}`); 
-        element.classList.add(`${newColor}`);
-        currentColor = newColor;
-    } )}
-    );
+        element.classList.add(`${newColor}`);    
+        currentColor = newColor;    
+    } ) } );
     return currentColor;
+    };
 };
 
-// Program starts running here 
+// Selects active button and disable the others
+function selectBrushButton(brush){
+    if ( brush == "black" ){
+        gridButtonBlack.classList.add("active");
+        gridButtonRainbow.classList.remove("active");
+        gridButtonEraser.classList.remove("active");
+        currentColor = "pixel_black";
+        return currentColor;
+    }
+    else if ( brush == "eraser" ){
+        gridButtonEraser.classList.add("active"); 
+        gridButtonBlack.classList.remove("active");
+        gridButtonRainbow.classList.remove("active");
+        currentColor = "pixel_eraser";
+        return currentColor;
+    }
+    else if ( brush == "rainbow" ){
+        gridButtonRainbow.classList.add("active");   
+        gridButtonEraser.classList.remove("active");
+        gridButtonBlack.classList.remove("active");
+        currentColor = "pixel_rainbow";
+        return currentColor;
+    }
+};
+
+//////////////////////////////////////////// Program starts running here 
 
 // Sets default starting color
 let currentColor = "pixel_black";
@@ -85,26 +123,28 @@ gridButtonClear.addEventListener("click", () => {
     clearGrid();
 });
 
-// Selects the eraser brush
-const gridButtonEraser = document.querySelector("#button_eraser");
-gridButtonEraser.addEventListener("click", () => {    
-    changeColor("pixel_eraser");
-    gridButtonBlack.classList.remove("active");
-    gridButtonEraser.classList.add("active");
-
-    
-});
-
 // Selects the black brush
 const gridButtonBlack = document.querySelector("#button_black");
 gridButtonBlack.addEventListener("click", () => {    
     changeColor("pixel_black");
-    gridButtonEraser.classList.remove("active");
-    gridButtonBlack.classList.add("active");
-    
+    selectBrushButton("black");
 });
+
+// Selects the eraser brush
+const gridButtonEraser = document.querySelector("#button_eraser");
+gridButtonEraser.addEventListener("click", () => {    
+    changeColor("pixel_eraser");
+    selectBrushButton("eraser");
+});
+
+// Selects the rainbow brush
+const gridButtonRainbow = document.querySelector("#button_rainbow");
+gridButtonRainbow.addEventListener("click", () => {    
+    changeColor("pixel_rainbow");
+    selectBrushButton("rainbow");
+}); 
 
 // Creates starting grid
 gridMaker(32);
-gridButtonEraser.classList.remove("active");
-gridButtonBlack.classList.add("active");
+changeColor("pixel_black");
+selectBrushButton("black");
